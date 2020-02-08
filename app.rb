618e9c6c53f89a6384dfdbf6b0b12928213a7ef3
Tim Smale <timothy.smale@oup.com>
2020-02-08 15:18:28 +0000

    Use sensible var names and separate out formatting

--- app.rb
+++ app.rb
@@ -1,38 +1,44 @@
 require 'sinatra'
 require 'sparql/client'
 
-def query_builder(key, value)
+def query_builder(search_type, search_term)
   prefix = "
     PREFIX dbo: <http://dbpedia.org/ontology/>
     PREFIX dbr: <http://dbpedia.org/resource/>
   "
   film_query = "
     SELECT DISTINCT ?actor WHERE {
-      dbr:#{value} dbo:starring ?actor 
+      dbr:#{search_term} dbo:starring ?actor 
     }
   "
   actor_query = "
     SELECT DISTINCT ?film WHERE {
       ?film rdf:type dbo:Film .
-      ?film dbo:starring dbr:#{value} .
+      ?film dbo:starring dbr:#{search_term} .
     }
   "
-  query = prefix + (key == 'film' ? film_query : actor_query)
+  query = prefix + (search_type == 'film' ? film_query : actor_query)
 end
 
 def query(query)
   sparql = SPARQL::Client.new("http://dbpedia.org/sparql")
   results = sparql.query(query)
+end
+
+def format_results(results)
+  array = []
   results.each_entry do |result|
     result.each_binding do |name, value|
       puts value
     end
   end
+  return array
 end
 
-get '/' do
-  key = params.keys[0]
-  value = params[key]
 
-  query(query_builder(key, value))
+get '/' do
+  search_type = params.keys[0]
+  search_term = params[search_type]
+
+  result = format_results(query(query_builder(search_type, search_term)))
 end
