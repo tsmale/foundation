1bbcb836a3452b22e3ef6fea237ca536e1afcd2b
Tim Smale <timothy.smale@oup.com>
2020-02-08 21:27:57 +0000

    Clean up formatting/syntax

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sparql/client'
 require 'json'
@@ -11,7 +13,7 @@ def query_builder(search_type, search_term)
   "
   film_query = "
     SELECT DISTINCT ?actor WHERE {
-      dbr:#{search_term} dbo:starring ?actor 
+      dbr:#{search_term} dbo:starring ?actor
     }
   "
   actor_query = "
@@ -20,39 +22,42 @@ def query_builder(search_type, search_term)
       ?film dbo:starring dbr:#{search_term} .
     }
   "
-  query = prefix + (search_type == 'film' ? film_query : actor_query)
+  prefix + (search_type == 'film' ? film_query : actor_query)
 end
 
 def query(query)
-  sparql = SPARQL::Client.new("http://dbpedia.org/sparql")
-  results = sparql.query(query)
+  sparql = SPARQL::Client.new('http://dbpedia.org/sparql')
+  sparql.query(query)
 end
 
 def format_results(results)
   array = []
   results.each_entry do |result|
-    array << result.bindings.values[0].to_s.sub('http://dbpedia.org/resource/','')
+    array << result.bindings.values[0].to_s.sub('http://dbpedia.org/resource/', '')
   end
   return array
 end
 
 get '/' do
-  types = ['actor', 'film']
+  if params.empty?
+    return ''
+  end
+
   search_type = params.keys[0]
   search_term = params[search_type]
-  cache_key = search_type + search_term
-
-  if not types.include?(search_type)
+  types = %w[actor film]
+  unless types.include?(search_type)
     return 'Invalid search type, expected "film" or "actor"'
   end
 
+  cache_key = search_type + search_term
   if $cache.key?(cache_key)
     response = $cache[cache_key]
   else
     result = format_results(query(query_builder(search_type, search_term)))
 
-    # Select opposite search type for response, pluralised
-    response_term = (types.select { |t| t != search_type })[0] + 's'
+    # Get the opposite search type for response and pluralise it
+    response_term = (types.reject { |t| t == search_type })[0] + 's'
     response = { response_term => result }.to_json
 
     $cache[cache_key] = response
