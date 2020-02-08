79c17ac2ceefe32d7ef4fc54298cc6dcde97e398
Tim Smale <timothy.smale@oup.com>
2020-02-08 15:15:16 +0000

    Separate out query builder

--- app.rb
+++ app.rb
@@ -1,24 +1,28 @@
 require 'sinatra'
 require 'sparql/client'
 
-def query(key, value)
-  sparql = SPARQL::Client.new("http://dbpedia.org/sparql")
-  film_query = "
+def query_builder(key, value)
+  prefix = "
     PREFIX dbo: <http://dbpedia.org/ontology/>
     PREFIX dbr: <http://dbpedia.org/resource/>
-    SELECT ?actor WHERE {
+  "
+  film_query = "
+    SELECT DISTINCT ?actor WHERE {
       dbr:#{value} dbo:starring ?actor 
     }
   "
   actor_query = "
-    PREFIX dbo: <http://dbpedia.org/ontology/>
-    PREFIX dbr: <http://dbpedia.org/resource/>
-    SELECT ?film WHERE {
+    SELECT DISTINCT ?film WHERE {
       ?film rdf:type dbo:Film .
       ?film dbo:starring dbr:#{value} .
     }
   "
-  results = sparql.query(key = 'film' ? film_query : actor_query)
+  query = prefix + (key == 'film' ? film_query : actor_query)
+end
+
+def query(query)
+  sparql = SPARQL::Client.new("http://dbpedia.org/sparql")
+  results = sparql.query(query)
   results.each_entry do |result|
     result.each_binding do |name, value|
       puts value
@@ -30,5 +34,5 @@ get '/' do
   key = params.keys[0]
   value = params[key]
 
-  query(key, value)
+  query(query_builder(key, value))
 end
